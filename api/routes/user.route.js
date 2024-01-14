61eb126a9ba259829db70a9973edfb2040115601
Umarkhon Akhmedov <umarkhonakhmedov2000@gmail.com>
2024-01-14 08:56:12 +0500

    Create update user API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,9 @@
 import express from "express";
+import { updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.json({
-    message: "Hello World",
-  });
-});
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
